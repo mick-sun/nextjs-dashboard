4c98e1f178d5cda6d3ca504a5d418a60c38a6747
sunwei <mick.sunwei@qq.com>
2024-04-30 20:00:15 +0800

    init

---
+++ app/layout.tsx
@@ -0,0 +1,11 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import { ReactNode } from 'react';
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
